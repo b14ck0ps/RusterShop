86090c3f8f8b0585b5b0c5d78784cb461f4ac659
Azran Hossain <mr69751@gmail.com>
2023-02-20 19:28:59 +0600

    show the details of a order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -9,17 +9,28 @@ namespace RusterShop.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly RusterEntities _db = new RusterEntities();
         // GET: Order
         public ActionResult Index()
         {
             List<Order> orders = new List<Order>();
-            using (RusterEntities db = new RusterEntities())
-            {
-                orders = db.Orders.Where(o => o.CustomerID == 1).ToList();
-            }
+            orders = _db.Orders.Where(o => o.CustomerID == 1).ToList();
             return View(orders);
         }
 
+        public ActionResult Details(int id)
+        {
+            Order order = _db.Orders.Find(id);
+            List<ProductsOrder> productsOrders = _db.ProductsOrders.Where(po => po.OrderID == id).ToList();
+            List<Product> products = new List<Product>();
+            foreach (ProductsOrder po in productsOrders)
+            {
+                Product product = _db.Products.Find(po.ProductID);
+                products.Add(product);
+            }
+            ViewBag.Order = order;
+            return View(products);
+        }
         public ActionResult Checkout()
         {
             var cart = (List<Product>)Session["Cart"];
@@ -27,27 +38,24 @@ namespace RusterShop.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-            using (RusterEntities db = new RusterEntities())
+            Order order = new Order
             {
-                Order order = new Order
-                {
-                    OrderDate = DateTime.Now,
-                    CustomerID = 1
+                OrderDate = DateTime.Now,
+                CustomerID = 1
 
+            };
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+
+            foreach (Product p in cart)
+            {
+                ProductsOrder po = new ProductsOrder
+                {
+                    OrderID = order.OrderID,
+                    ProductID = p.ProductID,
                 };
-                db.Orders.Add(order);
-                db.SaveChanges();
-
-                foreach (Product p in cart)
-                {
-                    ProductsOrder po = new ProductsOrder
-                    {
-                        OrderID = order.OrderID,
-                        ProductID = p.ProductID,
-                    };
-                    db.ProductsOrders.Add(po);
-                }
-                db.SaveChanges();
+                _db.ProductsOrders.Add(po);
+                _db.SaveChanges();
             }
             Session.Clear();
             return RedirectToAction("Index");
