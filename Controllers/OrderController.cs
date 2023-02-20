3909418d39d8b0248f418f39e02ea055bfd62200
Azran Hossain <mr69751@gmail.com>
2023-02-20 18:37:50 +0600

    update and show the orders

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using RusterShop.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RusterShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            List<Order> orders = new List<Order>();
+            using (RusterEntities db = new RusterEntities())
+            {
+                orders = db.Orders.Where(o => o.CustomerID == 1).ToList();
+            }
+            return View(orders);
+        }
+
+        public ActionResult Checkout()
+        {
+            var cart = (List<Product>)Session["Cart"];
+            if (cart == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (RusterEntities db = new RusterEntities())
+            {
+                Order order = new Order
+                {
+                    OrderDate = DateTime.Now,
+                    CustomerID = 1
+
+                };
+                db.Orders.Add(order);
+                db.SaveChanges();
+
+                foreach (Product p in cart)
+                {
+                    ProductsOrder po = new ProductsOrder
+                    {
+                        OrderID = order.OrderID,
+                        ProductID = p.ProductID,
+                    };
+                    db.ProductsOrders.Add(po);
+                }
+                db.SaveChanges();
+            }
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
